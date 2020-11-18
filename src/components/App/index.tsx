3b055dd16557b49392f163355edf5507372ea322
Rob LaFeve <rob@crema.us>
2020-11-18 15:21:23 -0600

    Initial commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react"
+import logo from "../../assets/logo.svg"
+import "./styles.css"
+
+export function App() {
+  const [inputState, setInputState] = useState("")
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/components/App/index.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <input
+          type="text"
+          className="App-input"
+          value={inputState}
+          onChange={(e) => setInputState(e.target.value)}
+        />
+      </header>
+    </div>
+  )
+}
